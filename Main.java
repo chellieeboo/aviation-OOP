38fc998aef42095ab2f26d1461066202d6951f3e
Rochelle Bayogbog <187154213+chellieeboo@users.noreply.github.com>
2026-09-15 05:31:41 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+/**
+ * Main.java
+ * Entry point of the Aviation Staff Management System.
+ * Creates one Employee (manager) and starts the menu.
+ */
 public class Main {
     public static void main(String[] args) {
-        // Create a manager (Employee) to start the menu
-        Employee manager = new Employee("Rochelle", 1001, "Operations", "Senior Manager", 50000.0);
+        Employee manager = new Employee("Rochelle Bayogbog", 6362, "Aviation Operations", "Senior Manager", 50000.00);
         manager.startMenu();
     }
 }
